c8609a029449f00efe54f8e283de5c46e04ef168
Jeffrey XIE <xwy5201314@gmail.com>
2020-08-12 09:29:58 -0400

    Upload the existing files of Python training

---
+++ Python/HelloPython.py
@@ -0,0 +1,39 @@
+msg = "Hello World"
+print(msg)
+
+str0 = "Elementary, my dear Watson."
+print(str0.rindex(' '))
+print(len(str0))
+print(str0[str0.rindex(' ') + 1 : len(str0) - 1])
+
+str1 = "one, two, three, four, five"
+print(type(str1))
+print(", appears:", str1.count(","))
+
+# Split a string into pieces
+str2 = str1.split(',')
+print(type(str2))
+print(str2)
+
+# ... without the whitespaces
+str3 = str1.split(', ')
+# or
+str3 = [x.strip() for x in str1.split(',')]
+print(str3)
+
+list1 = [70, 25, 99, 100, 88]
+list2 = list(list1)
+list2.append(66)
+list2.append(54)
+list2.append(99)
+print(list1)
+print(list2)
+
+list3 = list(list1)
+list3.extend([66, 54, 99])
+print(list1)
+print(list3)
+
+range1 = range(1, 10, 2)
+for i in range1:
+  print(i)
---
+++ Python/PackagesInPython.py
@@ -0,0 +1,12 @@
+import numpy # Importing the numpy package 
+
+vector = numpy.array([1,3,5,7]) # array function from numpy package
+print(vector)
+
+# Accessing single element
+print(vector[0])
+print(vector[-2])
+
+# Modify an element
+vector[-1]=9
+print(vector)
